4d780bbea6bdf8197324b22a6e652b02d0c7f5d5
Jorge Espindola <jlespindolaf@gmail.com>
2023-01-24 11:35:54 -0300

    Web: create New Habit function

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from 'react';
 import * as Checkbox from '@radix-ui/react-checkbox'
 import { Check } from "phosphor-react";
+import { api } from '../lib/axios';
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
@@ -8,11 +9,22 @@ export function NewHabitForm() {
     const [title, setTitle] = useState('')
     const [weekDays, setWeekDays] = useState<number[]>([])
 
-    function createNewHabit(event: FormEvent) {
+    async function createNewHabit(event: FormEvent) {
         event.preventDefault()
         
-        console.log(title)
-        console.log(weekDays)
+        if (!title || weekDays.length === 0) {
+            return
+        } else {
+            await api.post('habits', {
+                title,
+                weekDays
+            })
+        }
+
+        setTitle('')
+        setWeekDays([])
+
+        alert('Hábito criado com sucesso!')
     }
 
     function handleToggleWeekDay(weekDay: number) {
@@ -42,6 +54,7 @@ export function NewHabitForm() {
                 placeholder="ex.: Exercícios dormir bem, etc..."
                 className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
                 autoFocus
+                value={title}
                 onChange={event => setTitle(event.target.value)}
             />
 
@@ -55,6 +68,7 @@ export function NewHabitForm() {
                         <Checkbox.Root
                             key={weekDay}
                             className='flex items-center gap-3 group'
+                            checked={weekDays.includes(index)}
                             onCheckedChange={() => handleToggleWeekDay(index)}
                         >
                             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
