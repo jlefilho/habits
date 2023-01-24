d5bcecdc800264f5fe4f643f6a0899439a08ac4a
Jorge Espindola <jlespindolaf@gmail.com>
2023-01-24 11:12:30 -0300

    Web: API consumming

--- web/src/components/HabitDayBlock.tsx
+++ web/src/components/HabitDayBlock.tsx
@@ -1,35 +1,41 @@
 import * as Popover from '@radix-ui/react-popover'
 import * as Checkbox from '@radix-ui/react-checkbox'
 
+import dayjs from 'dayjs'
 import clsx from 'clsx'
 import { Check } from 'phosphor-react'
+
 import { ProgressBar } from './ProgressBar'
 
 interface HabitDayBlockProps {
-    completed: number
-    amount: number
+    date: Date
+    completed?: number
+    amount?: number
 }
 
-export function HabitDayBlock({ amount, completed }: HabitDayBlockProps) {
-    const completedPercentage = Math.round(completed / amount) * 100
+export function HabitDayBlock({ amount = 0, completed = 0, date}: HabitDayBlockProps) {
+    const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
+
+    const dayAndMonth = dayjs(date).format('DD/MM')
+    const dayOfWeek = dayjs(date).format('dddd')
 
     return (
         <Popover.Root>
             <Popover.Trigger
                 className={clsx('w-10 h-10 rounded-lg', {
-                    'bg-violet-500 border-violet-400' : completedPercentage >= 80,                 
-                    'bg-violet-600 border-violet-500' : completedPercentage >= 60 && completedPercentage < 80,               
-                    'bg-violet-700 border-violet-500' : completedPercentage >= 40 && completedPercentage < 60,               
+                    'bg-zinc-900 border-zic-800' : completedPercentage === 0,           
+                    'bg-violet-900 border-violet-700' : completedPercentage > 0 && completedPercentage < 20,        
                     'bg-violet-800 border-violet-600' : completedPercentage >= 20 && completedPercentage < 40,              
-                    'bg-violet-900 border-violet-700' : completedPercentage >= 0 && completedPercentage < 20,            
-                    'bg-zinc-900 border-zic-800' : completedPercentage === 0           
+                    'bg-violet-700 border-violet-500' : completedPercentage >= 40 && completedPercentage < 60,               
+                    'bg-violet-600 border-violet-500' : completedPercentage >= 60 && completedPercentage < 80,               
+                    'bg-violet-500 border-violet-400' : completedPercentage >= 80,                 
                 })}
             />
 
             <Popover.Portal>
                 <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-                    <span className='font-semibold text-zinc-400'>terça-feira</span>
-                    <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+                    <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
+                    <span className='mt-1 font-extrabold leading-tight text-3xl'>{dayAndMonth}</span>
 
                     <ProgressBar progress={completedPercentage}/>
 
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs"
+import { useEffect, useState } from "react"
+import { api } from "../lib/axios"
 import { generateDatesFromYearBeginning } from "../utils/generate-days-from-year-beginning.ts"
 import { HabitDayBlock } from "./HabitDayBlock"
 
@@ -8,7 +11,22 @@ const summaryDates = generateDatesFromYearBeginning()
 const minDayBlocks = 18 * 7 //18 weeks * 7 days
 const amountOfDaysToFill = minDayBlocks - summaryDates.length
 
+type Summary = {
+    id: string
+    date: string
+    amount: number
+    completed: number
+}[]
+
 export function SummaryTable() {
+    const [summary, setSummary] = useState<Summary>([])
+
+    useEffect(() => {
+        api.get('/summary').then(response => {
+            setSummary(response.data)
+        })
+    }, [])
+
     return (
         <div className="w-full flex ">
             {/* WEEKDAY HEADER */}
@@ -28,11 +46,16 @@ export function SummaryTable() {
             {/* DAY BLOCKS */}
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    })
+
                     return (
                         <HabitDayBlock
                             key={date.toString()}
-                            amount={10}
-                            completed={Math.round(Math.random() * 5)}
+                            date={date}
+                            amount={dayInSummary?.amount}
+                            completed={dayInSummary?.completed}
                         />
                     )
                 })}
